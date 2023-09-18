bc8cc8f788ec04f05b3b0907479774ad543f9111
Jumoke Olaleye <jumokeolaleye0@gmail.com>
2023-09-18 06:42:21 +0100

    Search text trim update

--- lib/resources/weather_api.dart
+++ lib/resources/weather_api.dart
@@ -2,10 +2,12 @@ import 'dart:convert';
 
 import "package:http/http.dart" as http;
 import 'package:sid_weather_app/models/weather.dart';
+
 ///calls the weather endpoint and returns the data
 Future<WeeklyWeather> fetchWeeklyWeather(String cityName) async {
-  final response = await http.get( Uri.parse(
-      'https://api.openweathermap.org/data/2.5/forecast/daily?q=$cityName&units=metric&appid=4d549679691769c85c1c782f4f20ce07'));
+  String newCity = cityName.trim();
+  final response = await http.get(Uri.parse(
+      'https://api.openweathermap.org/data/2.5/forecast/daily?q=$newCity&units=metric&appid=4d549679691769c85c1c782f4f20ce07'));
   if (response.statusCode == 200) {
     return WeeklyWeather.fromJson(jsonDecode(response.body));
   } else {
